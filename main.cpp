f5069259a4ec4a6dd5c46eda6f691f562ac089cb
LatekVon <latkaignacy@gmail.com>
2025-10-08 02:39:58 +0200

    convert opencv to raylib, stream into raylib display

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <raylib.h>
 
-#include <opencv2/alphamat.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <print>
 
 const int TARGET_FPS = 20;
 
@@ -9,16 +11,46 @@ int main() {
   const int screenHeight = 450;
 
   InitWindow(screenWidth, screenHeight, "debug display");
-
   SetTargetFPS(TARGET_FPS);
+
+  cv::VideoCapture video("...");
+
+  if (!video.isOpened()) {
+    std::println("Error - Could not open video!");
+    return -1;
+  }
+
+  cv::Mat cvFrame;
+  Image rayImage;
+
+  video.read(cvFrame);
+
+  rayImage.mipmaps = 1;
+  rayImage.format = PIXELFORMAT_UNCOMPRESSED_R8G8B8;
 
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(RAYWHITE);
 
-    DrawText("Hello world.", 190, 200, 20, LIGHTGRAY);
+    video.read(cvFrame);
 
+    // use color channel translation if all else fails:
+    cv::cvtColor(cvFrame, cvFrame, cv::COLOR_BGR2RGB);
+
+    // remap OpenCV to raylib image, no copy, using shared memory
+    // FIXME: size data not available until ~10 frames
+    rayImage.data = cvFrame.data;
+    rayImage.width = cvFrame.cols;
+    rayImage.height = cvFrame.rows;
+    SetWindowSize(cvFrame.cols, cvFrame.rows);
+
+    Texture2D texture = LoadTextureFromImage(rayImage);
+
+    DrawTexture(texture, 0, 0, WHITE);
+    DrawText("Hello world.", 20, 20, 20, GREEN);
     EndDrawing();
+
+    UnloadTexture(texture);
   }
 
   CloseWindow();
