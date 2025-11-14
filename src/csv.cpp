3569b0ac6f3c90c95a9c05e8155b615571bb4942
Ignacy Łątka <latkaignacy@gmail.com>
2025-11-14 14:22:40 +0100

    add csv support

---
+++ src/csv.cpp
@@ -0,0 +1,37 @@
+#include "csv.h"
+
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+std::vector<std::string> splitString(std::string& str, char sep) {
+  std::stringstream ss(str);
+  std::string temp;
+  std::vector<std::string> out;
+
+  while (getline(ss, temp, sep)) {
+    out.push_back(temp);
+  }
+
+  return out;
+}
+
+std::vector<std::vector<std::string>> loadCsv(std::string filename) {
+  // NOTE: We accept `#` for comments
+  std::vector<std::vector<std::string>> out;
+  std::string line;
+  std::ifstream file(filename);
+
+  if (file.is_open()) {
+    while (getline(file, line)) {
+      if (line[0] != '#') {
+        out.push_back(splitString(line, ','));
+      }
+    }
+
+    file.close();
+  }
+
+  return out;
+}
---
+++ src/csv.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <string>
+#include <vector>
+
+std::vector<std::vector<std::string>> loadCsv(std::string filename);
