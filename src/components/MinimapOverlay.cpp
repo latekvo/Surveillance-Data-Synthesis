e02a18d134fcc44ab18b3859acfaa2f3f1162eb1
LatekVon <latkaignacy@gmail.com>
2025-12-16 03:13:19 +0100

    add initial camera-real conversion

--- src/components/MinimapOverlay.cpp
+++ src/components/MinimapOverlay.cpp
@@ -16,22 +16,23 @@ void MinimapOverlay::draw() {
   float baryScale = 100;
 
   // TODO: Also draw camera bounds (0:0/1920:1080) on the minimap
+  // FIXME: The coords are currently used as px values, obv this is wrong
 
-  Triangle<float> bTrig{toBarycentric(this->coordMapPtr->cameraTrig.a,
-                                      this->coordMapPtr->cameraTrig),
-                        toBarycentric(this->coordMapPtr->cameraTrig.b,
-                                      this->coordMapPtr->cameraTrig),
-                        toBarycentric(this->coordMapPtr->cameraTrig.c,
-                                      this->coordMapPtr->cameraTrig)};
+  CoordMap& map = *this->coordMapPtr;
+  Triangle<float>& cTrig = map.cameraTrig;
 
-  bTrig = bTrig * baryScale + baryScale;
+  Triangle<float> oTrig{
+      remapPointToReal(cTrig.a, map),
+      remapPointToReal(cTrig.b, map),
+      remapPointToReal(cTrig.c, map),
+  };
 
-  DrawText("A", bTrig.a.x, bTrig.a.y, 16, PINK);
-  DrawText("B", bTrig.b.x, bTrig.b.y, 16, PINK);
-  DrawText("C", bTrig.c.x, bTrig.c.y, 16, PINK);
+  DrawText("A", oTrig.a.x, oTrig.a.y, 16, PINK);
+  DrawText("B", oTrig.b.x, oTrig.b.y, 16, PINK);
+  DrawText("C", oTrig.c.x, oTrig.c.y, 16, PINK);
 
-  const Vector2 baVertexes[4] = {bTrig.a.toRaylib(), bTrig.b.toRaylib(),
-                                 bTrig.c.toRaylib(), bTrig.a.toRaylib()};
+  const Vector2 baVertexes[4] = {oTrig.a.toRaylib(), oTrig.b.toRaylib(),
+                                 oTrig.c.toRaylib(), oTrig.a.toRaylib()};
 
   DrawLineStrip(baVertexes, 4, WHITE);
 
@@ -41,8 +42,7 @@ void MinimapOverlay::draw() {
     AS::Point feet = AS::Point{rawRect.x + rawRect.width / 2 - 1,
                                rawRect.y + rawRect.height};
 
-    AS::Point bFeet = toBarycentric(feet, this->coordMapPtr->cameraTrig);
-    bFeet = bFeet * baryScale + baryScale;
+    AS::Point bFeet = remapPointToReal(feet, map);
 
     DrawRectangleLinesEx({bFeet.x, bFeet.y, 4, 4}, 3.f, PINK);
   }
